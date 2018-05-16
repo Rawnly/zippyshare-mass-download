57471126a7a6c1cf4b481d1560b64370882f99ba
Federico Vitale <federicovitale@Federicos-iMac.local>
2018-05-16 11:06:55 +0200

    fix

--- app/libs/core.js
+++ app/libs/core.js
@@ -1,31 +1,35 @@
-const fs = require('fs');
+const fs = require("fs");
+const chalk = require("chalk");
+
 const Ora = require("ora");
-const path = require('path');
+const path = require("path");
 const File = require("simple-download");
-const printBlock = require('@splash-cli/print-block');
-const pf = require('@splash-cli/path-fixer')
-const mkdir = require('mkdirp');
+const printBlock = require("@splash-cli/print-block");
+const pf = require("@splash-cli/path-fixer");
+const mkdir = require("mkdirp");
 
 const {
 	round,
 	getFile,
 	getTime,
-	averages,
+	average,
 	readJson,
 	getFileName,
-	getDifference,
-} = require('./utils')
-
+	getDifference
+} = require("./utils");
 
 const spinner = new Ora();
 
-module.exports = async function (episodes, destination = path.join(__dirname, 'downloads')) {
+module.exports = async function (
+	episodes,
+	destination = path.join(__dirname, "downloads")
+) {
 	destination = pf(destination);
-	episodes = readJson(pf(episodes))
-	
+	episodes = readJson(pf(episodes));
+
 	mkdir.sync(destination);
 
-	let files = fs.readdirSync(destination).filter(f => f.includes('.rar')),
+	let files = fs.readdirSync(destination).filter(f => f.includes(".rar")),
 		urls = [],
 		times = [],
 		avTime = 0,
@@ -34,15 +38,23 @@ module.exports = async function (episodes, destination = path.join(__dirname, 'd
 		action = {
 			start: Date.now(),
 			elapsedTime: 0
-		}
+		};
 
 	if (files.length) {
-		printBlock(chalk`Found: {green ${files.length-1}} rar files. Counting from {green ${files.length-1}}`, '')
+		printBlock(
+			chalk `Found: {green ${files.length -
+        1}} rar files. Counting from {green ${files.length - 1}}`,
+			""
+		);
+	} else {
+		files.push('');
 	}
 
-	spinner.start(chalk`{dim [ETA: calculating...]} {yellow ${(urls.length + files.length - 1)}} of {yellow ${episodes.length}}`);
+	spinner.start(
+		chalk `{dim [ETA: calculating...]} {yellow ${urls.length + files.length - 1}} of {yellow ${episodes.length}}`
+	);
 
-	for (let i = files.length ? files.length - 1 : 0; i < episodes.length; i++) {
+	for (let i = files.length - 1; i < episodes.length; i++) {
 		const element = episodes[i].replace(/\s+/, "");
 
 		try {
@@ -57,9 +69,15 @@ module.exports = async function (episodes, destination = path.join(__dirname, 'd
 
 			times.push(seconds);
 			avTime = average.apply(average, times);
-			eta = getDifference(round(avTime * (episodes.length - (urls.length + files.length - 1)), 0));
+			eta = getDifference(
+				round(
+					avTime *
+					(episodes.length - urls.length + files.length - 1),
+					0
+				)
+			);
 
-			spinner.text = chalk`{dim [ETA: ${eta.hours}h ${eta.minutes}m ${eta.seconds}s]} {yellow ${(urls.length + files.length - 1)}} of {yellow ${episodes.length}}`;
+			spinner.text = chalk `\n{dim [SINGLE: ${getDifference(avTime).hours}h ${getDifference(avTime).minutes}m ${getDifference(avTime).seconds}s]\n[ETA: ${eta.hours}h ${eta.minutes}m ${eta.seconds}s]}\n{yellow ${urls.length + files.length - 1}} of {yellow ${episodes.length}}`;
 		} catch (error) {
 			throw error;
 		}
@@ -67,13 +85,12 @@ module.exports = async function (episodes, destination = path.join(__dirname, 'd
 
 	action.elapsedTime = getDifference(action.start, Date.now());
 
-	spinner.info(`Completed in ${
-		round(action.elapsedTime.hours, 0)
-	}h ${
-		round(action.elapsedTime.minutes, 0)
-	}m ${
-		round(action.elapsedTime.seconds, 0)
-	}s`);
+	spinner.info(
+		`Completed in ${round(action.elapsedTime.hours, 0)}h ${round(
+      action.elapsedTime.minutes,
+      0
+    )}m ${round(action.elapsedTime.seconds, 0)}s`
+	);
 
 	spinner.succeed();
-}
+};
